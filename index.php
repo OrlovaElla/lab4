caa7134e21e901d1055d6b1daee7ed41dabcd4e4
OrlovaElla <118119403+OrlovaElla@users.noreply.github.com>
2022-12-27 20:49:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $xml = simplexml_load_file("data.xml") or die("Error: Cannot create object");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
